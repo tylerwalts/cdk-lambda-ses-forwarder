d9cf9e5b9f2e1402e38ac466298dbc8e6c2a14b0
tylerwalts <git@tylerwalts.com>
2026-06-08 11:03:21 -0700

    CDK Upgrade and redeploy of spam fix. (#3)

--- bin/ses.ts
+++ bin/ses.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { SesStack } from '../lib/ses-stack';
 import { config } from "../lambda/config";
 
 const app = new cdk.App();
-new SesStack(app, `SesForwarder${config.project}`);
+new SesStack(app, `NewSesForwarder${config.project}`, {
+  env: { account: '680397041807', region: 'us-west-2' }
+});
---
+++ lambda/config.ts
@@ -0,0 +1,41 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+export const enum SpamFilterOption {
+  NONE,
+  DEFAULT,
+  CUSTOM
+}
+
+const configDir = path.join(__dirname, 'config');
+
+function loadList(filename: string): string[] {
+  const filepath = path.join(configDir, filename);
+  return fs.readFileSync(filepath, 'utf8')
+    .split('\n')
+    .map((line: string) => line.trim())
+    .filter((line: string) => line.length > 0);
+}
+
+export const config = {
+  project: "TylerWalts",
+  domain: "tylerwalts.com",
+  recipient: "catch-all@tylerwalts.com",
+  headerValue: "forwarded",
+  emailKeyPrefix: "emails/",
+  subjectPrefix: "[tyler] ",
+  allowPlusSign: true,
+  spamFilter: SpamFilterOption.CUSTOM,
+
+  subjectFilterKeywords: loadList('subjectFilterKeywords.txt'),
+  blockedRecipients: loadList('blockedRecipients.txt'),
+  blockedSenders: loadList('blockedSenders.txt'),
+  blockedSenderDomains: loadList('blockedSenderDomains.txt'),
+  bulkMailHeaders: loadList('bulkMailHeaders.txt'),
+  brandKeywords: loadList('brandKeywords.txt'),
+  trustedBrandDomains: loadList('trustedBrandDomains.txt'),
+
+  forwardMapping: {
+    "@tylerwalts.com": ["tylerwalts@gmail.com"],
+  } as { [key: string]: string[] }
+};
